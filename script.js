747fce661e3c418ab17a8e794ee0843465234fd2
mathiasur <mathias.urtheil@gmail.com>
2023-01-06 01:43:53 -0300

    Add function to create new book objects

--- script.js
+++ script.js
@@ -4,16 +4,53 @@ const $frmNewBook = document.querySelector("#frm-book");
 const $btnCancel = document.querySelector("#nb-cancel");
 const $btnSave = document.querySelector("#nb-save");
 
-const openForm = function openNewBookForm() {
+const myLibrary = [];
+
+const openForm = () => {
   $newBook.style = "display: flex";
 };
 
-const closeForm = function closeNewBookForm() {
+$btnAdd.addEventListener("click", openForm);
+
+const closeForm = () => {
+  $frmNewBook.reset();
   $newBook.style = "display: none";
 };
 
-$btnAdd.addEventListener("click", openForm);
-$btnCancel.addEventListener("click", () => {
-  $frmNewBook.reset();
+$btnCancel.addEventListener("click", closeForm);
+
+const Book = (title, author, pages, isRead) => {
+  const toggle = function changeStatusValue() {
+    if (this.isRead === true) {
+      this.isRead = false;
+    } else {
+      this.isRead = true;
+    }
+  };
+
+  return {
+    title,
+    author,
+    pages,
+    isRead,
+    toggle,
+  };
+};
+
+const newBook = () => {
+  const $txtTitle = document.querySelector("#title").value;
+  const $txtAuthor = document.querySelector("#author").value;
+  const $txtPages = document.querySelector("#pages").value;
+  const $slctStatus = document.querySelector("#status").value;
+
+  const book = Book($txtTitle, $txtAuthor, $txtPages, $slctStatus);
+  return book;
+};
+
+const saveBook = () => myLibrary.push(newBook());
+
+$btnSave.addEventListener("click", (event) => {
+  saveBook();
   closeForm();
+  event.preventDefault();
 });
