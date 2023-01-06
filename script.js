99664f6ea4cbb92c966b74239e6d13197bb28df0
mathiasur <mathias.urtheil@gmail.com>
2023-01-05 23:38:23 -0300

    Add function to open new book form

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+const $btnAdd = document.querySelector("#add");
+const $newBook = document.querySelector("#new-book");
+const $frmNewBook = document.querySelector("#frm-book");
+const $btnCancel = document.querySelector("#nb-cancel");
+const $btnSave = document.querySelector("#nb-save");
 
+const openForm = function openNewBookForm() {
+  $newBook.style = "display: flex";
+};
 
+const closeForm = function closeNewBookForm() {
+  $newBook.style = "display: none";
+};
+
+$btnAdd.addEventListener("click", openForm);
+$btnCancel.addEventListener("click", () => {
+  $frmNewBook.reset();
+  closeForm();
+});
